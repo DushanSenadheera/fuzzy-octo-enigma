18efc29e46dace77687d1907dd45dc8529638fe3
Dushan Senadheera <dcsenadheera777@gmail.com>
2023-12-18 19:14:32 +0530

    update: vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //run on localhost
   server: {
-    host: 'localhost',
-    port: 3000,
+    host: true,
+    port: 8000,
   },
-})
+  preview: {
+    host: true,
+    port: 8000,
+  },
+});
