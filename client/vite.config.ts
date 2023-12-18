d3b5663e4392d31e0e8c92b079f1ddfcbc595c9a
Dushan Senadheera <dcsenadheera777@gmail.com>
2023-12-18 19:07:14 +0530

    update config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,17 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  //run on localhost
   server: {
-    //host in localhost
-    host: '0.0.0.0',
-    port: 8000,
-    // port in localhost
-    // This is the port which we will use in docker
-    // Thanks @sergiomoura for the window fix
-    // add the next lines if you're using windows and hot reload doesn't work
-     watch: {
-       usePolling: true
-     }
-  }
+    host: 'localhost',
+    port: 3000,
+  },
 })
